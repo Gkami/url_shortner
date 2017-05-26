b461ea35940b919dceb08bf316158dcef3c68ad9
Your Name <you@example.com>
2017-05-26 11:35:28 +0800

    shorten the url

--- app/controllers/urls_controller.rb
+++ app/controllers/urls_controller.rb
@@ -5,8 +5,27 @@ def index
 end
 
 def show
-	byebug
 	@url = Url.find(params[:id])
+end
+
+def new
+	@url = Url.new
+end
+
+def create
+  #@url = Url.new(params[:url])
+  @url=Url.new(url_params)
+  @url.shorten
+  if @url.save
+      redirect_to @url  
+  else
+      render 'new'
+  end   
+end
+
+private 
+def url_params
+    params.require(:url).permit(:long_url)
 end
 
 
--- app/models/url.rb
+++ app/models/url.rb
@@ -1,4 +1,30 @@
 class Url < ApplicationRecord
 	validates :long_url, format: { with: /\A(https|http):\/\//} 
 
+before_save :shorten
+
+	def shorten
+		r = ""  # empty string
+
+		# 7.times{ r += rand (10).to_s}  just the numbers
+
+		# range = [*'0'..'9',*'A'..'Z', *'a'..'z']    another method
+		 # @short_url = (6...0).map {range.sample}.join
+
+		7.times do |alphanum|
+			case  rand (3)
+			when 0
+				r += (97 + rand(25)).chr
+			when 1
+
+				r += (65 + rand(25)).chr
+			when 2
+				r += rand(10).to_s
+			end  
+		end
+		
+		self.short_url = r
+
+	end
+
 end
