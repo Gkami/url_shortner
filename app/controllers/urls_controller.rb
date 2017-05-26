e8eb7ab2a7bea9a4e06445a4ea07344561b48023
Your Name <you@example.com>
2017-05-26 11:44:22 +0800

    added destroy

--- app/controllers/urls_controller.rb
+++ app/controllers/urls_controller.rb
@@ -23,10 +23,19 @@ def create
   end   
 end
 
-private 
+def destroy
+    @url = Url.find(params[:id])
+    @url.destroy
+    redirect_to urls_path
+end
+
+
+private
+ 
 def url_params
     params.require(:url).permit(:long_url)
 end
+
 
 
 
